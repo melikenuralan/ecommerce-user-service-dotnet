9cef9635cabe8c8827ca75d2b264b6a2be2c58c6
Melikenur Alan <alanmelikenur0@gmail.com>
2025-05-12 21:51:00 +0300

    Initial commit: Clean history

---
+++ README.md
@@ -0,0 +1,2 @@
+# ecommerce-user-service-dotnet
+User microservice for handling authentication, registration, and identity management in a distributed e-commerce system. Built with ASP.NET Core, EF Core, and Clean Architecture.
---
+++ src/Infrastructure/UserService.Persistence/Data/UserServiceDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UserService.Persistence.Identity;
+
+namespace UserService.Persistence.Data
+{
+    public class UserServiceDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+    {
+        public UserServiceDbContext(DbContextOptions<UserServiceDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+        }
+    }
+}
+
+
+/* 
+ * isimleri özelleştirmek istersek --> builder.Entity<AppUser>().ToTable("Users");
+            builder.Entity<AppRole>().ToTable("Roles");
+            builder.Entity<IdentityUserRole<Guid>>().ToTable("UserRoles");
+            builder.Entity<IdentityUserClaim<Guid>>().ToTable("UserClaims");
+            builder.Entity<IdentityUserLogin<Guid>>().ToTable("UserLogins");
+            builder.Entity<IdentityRoleClaim<Guid>>().ToTable("RoleClaims");
+            builder.Entity<IdentityUserToken<Guid>>().ToTable("UserTokens");*/
